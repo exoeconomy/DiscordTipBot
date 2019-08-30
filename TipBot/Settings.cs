1c08421b2c95277ce430c7c396cf8cb56564239c
turcol <turcol@gmail.com>
2019-08-30 19:58:37 -0400

    Ticker, botprefix, supportUser are now parameters

--- TipBot/Settings.cs
+++ TipBot/Settings.cs
@@ -10,6 +10,14 @@ namespace TipBot
 
             this.BotToken = configReader.GetOrDefault<string>("token", "$TOKEN");
 
+            this.Ticker = System.Environment.GetEnvironmentVariable("ticker");
+
+            this.BotOptionalPrefix = System.Environment.GetEnvironmentVariable("botprefix");
+
+            this.SupportUsername = System.Environment.GetEnvironmentVariable("supportUsername");
+
+            this.SupportDiscriminator = System.Environment.GetEnvironmentVariable("supportDiscriminator");
+            
             this.EnableMigrations = configReader.GetOrDefault<bool>("enableMigrations", true);
 
             this.ConnectionString = configReader.GetOrDefault<string>("connectionString", @"Data Source=localhost;Initial Catalog=testBotDb;user id=sa;password=password;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
@@ -19,7 +27,7 @@ namespace TipBot
 
         public string BotToken { get; private set; }
 
-        public string Ticker { get; } = "EXOS";
+        public string Ticker { get; private set; }
 
         public uint PregeneratedAddressesCount { get; } = 8000;
 
@@ -41,13 +49,13 @@ namespace TipBot
         public bool EnableMigrations { get; private set; }
 
         /// <summary>Nickname of a user that will receive a message in case of a fatal error.</summary>
-        public string SupportUsername { get; } = "Nayib";
-        public string SupportDiscriminator { get; } = "8583";
+        public string SupportUsername { get; private set; }
+        public string SupportDiscriminator { get; private set; }
 
         /// <summary>DB connection string. Local db will be used if it's null.</summary>
         public string ConnectionString { get; private set; }
 
-        public string BotOptionalPrefix { get; } = "EXOS-TipBot";
+        public string BotOptionalPrefix { get; private set; }
 
         /// <summary>Default time in which self destructed messages are deleted.</summary>
         public int SelfDestructedMessagesDelaySeconds { get; } = 20;
